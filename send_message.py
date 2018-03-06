97f044298cb98577daffc87e789657e816efc3ec
Abhilasha Jha <abhilashajha96@gmail.com>
2018-03-06 19:58:49 +0530

    Oops

--- send_message.py
+++ send_message.py
@@ -11,8 +11,8 @@ from twilio.rest import Client
 ser=serial.Serial('/dev/ttyACM1', 9600)
 sensor1=[]
 sensor2=[]
-account_sid="AC596b5e9e590d616f60a0e0480eebcd18"
-auth_token="6c9e5614497974308fda16f2e8033f31"
+account_sid="abcd"
+auth_token="abcd"
 twilioclient=Client(account_sid, auth_token)
 def sendMessage(myTwilioNumber,destCellPhone,message):
     twilioclient.messages.create(to=destCellPhone,from_=myTwilioNumber,body=message)
@@ -23,15 +23,15 @@ for i in range(0,3):
     if(i%2!=0):
         sensor1.append(pro_data)
         if pro_data>1.50:
-            sendMessage("14159354987","+919790034764","Valve 1 is on")
+            sendMessage("NumTwiliogives","YourNum","Valve 1 is on")
         else:
-            sendMessage("14159354987","+919790034764","Valve 1 is off")
+            sendMessage("NumTwiliogives","YourNum","Valve 1 is off")
     else:
         sensor1.append(pro_data)
         if pro_data>1.50:
-            sendMessage("14159354987","+919790034764","Valve 2 is on")
+            sendMessage("NumTwiliogives","YourNum","Valve 2 is on")
         else:
-            sendMessage("14159354987","+919790034764","Valve 2 is off")
+            sendMessage("NumTwiliogives","YourNum","Valve 2 is off")
         
         
     
